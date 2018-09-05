481ab6a924a2e009cc8f2ddb17c73b6c681a5c48
ioanzicu <zicu.ioan@mail.ru>
2018-09-05 10:59:39 +0100

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -13,15 +13,14 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
 	console.log('New user connected');
-
-	socket.emit('newMessage', {
-		from: 'Iro',
-		text: 'Hey, What is going on',
-		createdAt: new Date().getTime()
-	});
-
+	
 	socket.on('createMessage', (message) => {
 		console.log('Create Message', message);
+		io.emit('newMessage', {
+			from: message.from,
+			text: message.text,
+			createdAt: new Date().getTime()
+		});
 	});
 
 	socket.on('disconnect', () => {

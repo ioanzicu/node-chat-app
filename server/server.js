6c1690626f796f1f879a07aa4f3189d68a525e3f
ioanzicu <zicu.ioan@mail.ru>
2018-09-05 11:27:02 +0100

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -13,14 +13,31 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
 	console.log('New user connected');
-	
+
+	socket.emit('newMessage', {
+		from: 'Admin',
+		text: 'Welcome to the chat app'
+	});
+
+	socket.broadcast.emit('newMessage', {
+		from: 'Admin',
+		text: 'New user joined'
+	});
+
 	socket.on('createMessage', (message) => {
 		console.log('Create Message', message);
+
 		io.emit('newMessage', {
 			from: message.from,
 			text: message.text,
 			createdAt: new Date().getTime()
 		});
+
+		// socket.broadcast.emit('newMessage', {
+		// 	from: message.from,
+		// 	text: message.text,
+		// 	createdAt: new Date().getTime()
+		// });
 	});
 
 	socket.on('disconnect', () => {

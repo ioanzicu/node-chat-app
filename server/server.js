dc149a5982d0fa284102105b52df7cd36d34c4ef
ioanzicu <zicu.ioan@mail.ru>
2018-09-06 17:29:26 +0100

    Add case insensitive room

--- server/server.js
+++ server/server.js
@@ -20,17 +20,19 @@ io.on('connection', (socket) => {
 	console.log('New user connected');
 
 	socket.on('join', (params, callback) => {
-		if (!isRealString(params.name) || !isRealString(params.room)) {
+		var room = params.room.toLowerCase();
+
+		if (!isRealString(params.name) || !isRealString(room)) {
 			return callback('Name and room name are required.');
 		}
 
-		socket.join(params.room);
+		socket.join(room);
 		users.removeUser(socket.id);
-		users.addUser(socket.id, params.name, params.room);
+		users.addUser(socket.id, params.name, room);
 
-		io.to(params.room).emit('updateUserList', users.getUserList(params.room));
+		io.to(room).emit('updateUserList', users.getUserList(room));
 		socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
-		socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined.`));
+		socket.broadcast.to(room).emit('newMessage', generateMessage('Admin', `${params.name} has joined.`));
 
 		callback();
 	});
--- server/utils/users.js
+++ server/utils/users.js
@@ -1,4 +1,3 @@
-
 class Users {
 	constructor () {
 		this.users = [];

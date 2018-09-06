a9bd70ec2117ba78317a40b83a868fc703ff9ae1
ioanzicu <zicu.ioan@mail.ru>
2018-09-06 11:16:02 +0100

    Add mustache.js for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -1,35 +1,43 @@
 var socket = io();
 
+// CONNECT
 socket.on('connect', function () {
 	console.log('Connected to server');
 
 });
 
+// DICONNECT
 socket.on('disconnect', function () {
 	console.log('Disconnected from server');
 });
 
+// NEW MESSAGE
 socket.on('newMessage', function (message) {
 	var formattedTime = moment(message.createdAt).format('h:mm a');
+	var template = jQuery('#message-template').html();
+	var html = Mustache.render(template, {
+		text: message.text,
+		from: message.from,
+		createdAt: formattedTime
+	});
 
-	var li = jQuery('<li></li>');
-	li.text(`${message.from} ${formattedTime}: ${message.text}`);
-
-	jQuery('#messages').append(li);
+	jQuery('#messages').append(html);
 });
 
+// NEW LOCATION
 socket.on('newLocationMessage', function (message) {
 	var formattedTime = moment(message.createdAt).format('h:mm a');
+	var template = jQuery('#location-message-template').html();
+	var html = Mustache.render(template, {
+		url: message.url,
+		from: message.from,
+		createdAt: formattedTime
+	});
 
-	var li = jQuery('<li></li>');
-	var a = jQuery('<a href="" target="_blank">My current location</a>');
-
-	li.text(`${message.from} ${formattedTime}: `);
-	a.attr('href', message.url);
-	li.append(a);
-	jQuery('#messages').append(li);
+	jQuery('#messages').append(html);
 });
 
+// FORM
 jQuery('#message-form').on('submit', function (e) {
 	e.preventDefault();
 
@@ -43,6 +51,7 @@ jQuery('#message-form').on('submit', function (e) {
 	});
 });
 
+// SEND LOCATION
 var locationButton = jQuery('#send-location');
 jQuery('#send-location').on('click', function () {
 	if (!navigator.geolocation) {
